12d910c80f610cdbec4caffa01d40c7f39a8951b
zhwujie <jay@hytto.com>
2021-08-16 21:35:12 +0800

    修改

--- react-staging/src/App.js
+++ react-staging/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1111111
+          Learn React1111111222222
         </a>
       </header>
     </div>
