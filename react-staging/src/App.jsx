e9df0ff014440649d56b8b94d087bfdc3304b897
“e <jay@hytto.com>
2021-09-16 19:09:15 +0800

    添加antd

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,35 +1,10 @@
 import React, {Component} from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom'
-import about from './pages/about'
-import home from './pages/home'
-import MyNavLink from './components/MyNavLink'
-import Header from './components/header'
-class App extends Component {
 
+class App extends Component {
     render() {
         return (
             <div>
-                <Header/>
-                <div className="row">
-                    <div className="col-xs-2 col-xs-offset-2">
-                        <div className="list-group">
-                            <MyNavLink to="/about">About</MyNavLink>
-                            <MyNavLink to="/home">Home</MyNavLink>
-                        </div>
-                    </div>
-                    <div className="col-xs-6">
-                        <div className="panel">
-                            <div className="panel-body">
-                                <Switch>
-                                    <Route path="/about" component={about}/>
-                                    <Route path="/home" component={home}/>
-                                    <Redirect to="/about" />
-                                </Switch>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                app....
             </div>
         );
     }
