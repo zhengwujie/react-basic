d31e0577126f4383f912f51eb16d6f5d145c5b47
“e <jay@hytto.com>
2021-09-17 12:25:25 +0800

    添加antd

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import {Button, DatePicker, Space} from "antd";
+import 'antd/dist/antd.css'
+import {AppleOutlined} from '@ant-design/icons'
 
 class App extends Component {
+    onChange = (date, dateString) => {
+        console.log(date, dateString);
+    }
+
     render() {
         return (
             <div>
                 app....
+                <Button type="primary">Primary button</Button>
+                <AppleOutlined/>
+                <Space direction="vertical">
+                    <DatePicker onChange={this.onChange}/>
+                    <DatePicker onChange={this.onChange} picker="week"/>
+                    <DatePicker onChange={this.onChange} picker="month"/>
+                    <DatePicker onChange={this.onChange} picker="quarter"/>
+                    <DatePicker onChange={this.onChange} picker="year"/>
+                </Space>
             </div>
         );
     }
